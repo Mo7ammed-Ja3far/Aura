de395f3dba61acbde0a77dd9dcf8aa62ab43e363
Mohamed Jafar <mo7ammed.ja3far@gmail.com>
2026-04-14 21:09:09 +0200

    Add Tailwind CSS integration and enhance Header component with navigation links
    - Installed Tailwind CSS and its Vite plugin.
    - Updated Header component to include navigation links and a hero image.
    - Modified global styles to incorporate Tailwind CSS.
    - Created a new Todo list file for project tracking.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,48 @@
 import React from "react";
+import hero from "../assets/hero.png";
+import { NavLink } from "react-router-dom"; // غيرنا Link لـ NavLink
 
 function Header() {
-  return <div>Header</div>;
+  const navList = [
+    { name: "home", key: "home", link: "/" },
+    { name: "all doctors", key: "all-doctors", link: "/doctors" },
+    { name: "about", key: "about", link: "/about" },
+    { name: "contact", key: "contact", link: "/contact" },
+  ];
+
+  return (
+    <header className="max-w-[1525px] border-b h-[86px] border-[#Adadad] mx-auto flex items-center justify-between">
+      <div className="hero relative left-[54px]">
+        <img src={hero} alt="hero" />
+      </div>
+
+      <nav className="relative left-[50px]">
+        <ul className="flex items-center uppercase gap-7">
+          {navList.map((item) => (
+            <li key={item.key}>
+              <NavLink
+                to={item.link}
+                className={({ isActive }) =>
+                  `relative text-sm select-none transition-all duration-300 hover:text-primary pb-2
+                  ${
+                    isActive
+                      ? "text-primary after:content-[''] after:absolute after:left-0 after:bottom-[-10px] after:w-full after:h-[1px] after:bg-primary"
+                      : "text-gray-600"
+                  }`
+                }
+              >
+                {item.name}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </nav>
+
+      <div className="bg-primary text-white px-4 py-2 rounded-3xl h-[54px] w-[195px] flex items-center justify-center cursor-pointer">
+        Create account
+      </div>
+    </header>
+  );
 }
 
 export default Header;
